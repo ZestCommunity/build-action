a4029b3b8271c2fdaf33554b64728f5595fc93b8
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2025-03-13 20:54:52 -0500

    refactor: :bug: Add logging for Meson setup and compile exit codes

--- build-tools/build.sh
+++ build-tools/build.sh
@@ -50,6 +50,7 @@ COMPILE_STD_OUTPUT=$(mktemp)
 echo "::group::Build Project"
 meson setup --cross-file scripts/v5.ini builddir | tee $COMPILE_STD_OUTPUT
 meson_exit_code = ${PIPESTATUS[0]}
+echo "Meson setup exit code: $meson_exit_code"
 if [ $meson_exit_code -ne 0 ]; then
   echo "Meson setup failed. Please check the logs for more information."
   GH_SUMMARY_OUTPUT=$(mktemp)
@@ -79,6 +80,7 @@ start_time=$(date +%s)
 meson compile -C builddir | tee $STD_OUTPUT
 end_time=$(date +%s)
 meson_exit_code=${PIPESTATUS[0]}
+echo "Meson compile exit code: $meson_exit_code"
 elapsed_time=$((end_time - start_time))
 echo "Meson compile took $elapsed_time seconds"
 STD_EDITED_OUTPUT=$(mktemp)
